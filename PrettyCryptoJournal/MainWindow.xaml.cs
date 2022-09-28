42f257be14025b79e634e69705452bfeb562b0c1
36m_901gs <njisom13@gmail.com>
2022-09-28 19:33:00 -0400

    Dumpin all my stuff
    Don't worry about it bro, just puttin all our hard work somewhere so it doesn't get wombo combo'd by life bro

---
+++ PrettyCryptoJournal/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ICSharpCode.AvalonEdit;
+
+namespace PrettyCryptoJournal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void openBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void scrambleBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void KeyBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textEditor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
